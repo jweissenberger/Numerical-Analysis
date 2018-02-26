0e18a43caaff925a34c1aba7fa3c3d2d9dbb47ff
John Weissenberger <johnaweissenberger@gmail.com>
2018-02-26 11:01:36 -0500

    Finish problem set 2

--- PolyEval/integral_approx.m
+++ PolyEval/integral_approx.m
@@ -1,13 +1,28 @@
 %Jack Weissenberger February 2018
 %integral_approx
+% This script approximates the definite integral of the function f using
+% polynomial interpolation and cubis splines interpolation 
 f = @(x) 1./(1+exp(x)+25.*x.^2);
 n = 3;
-X = linspace(-1,1,n);
-Y = f(X);
-P = polyfit(X,Y,2);
-q = polyint(P);
-I = diff(polyval(q,[-1 1]))
+i = 1;
 
-pp = spline(X, Y,X);
-qq = polyint(pp);
-II = diff(polyval(qq,[-1 1]))
+while n < 10,
+    X = linspace(-1,1,n);
+    Y = f(X);
+    
+    %polynomial interpolation
+    P = polyfit(X,Y,2);
+    q = polyint(P);
+    polyI(1, i)= diff(polyval(q,[-1 1]));
+
+    %cubic spline interpolation
+    pp = spline(X, Y,X);
+    qq = polyint(pp);
+    cubicSI(1, i) = diff(polyval(qq,[-1 1]));
+    
+    i = i +1;
+    n = n+2;
+end
+
+polyI
+cubicSI
