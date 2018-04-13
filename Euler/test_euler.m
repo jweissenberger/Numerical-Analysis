d94b90f1dc6c4a48d5d40e08785e4d36b7ba3f84
John Weissenberger <johnaweissenberger@gmail.com>
2018-04-12 21:11:24 -0400

    add test euler

---
+++ Euler/test_euler.m
@@ -0,0 +1,24 @@
+%Jack Weissenberger
+% test euler
+
+f = @(t, y) -10.*(y-cos(t))-sin(t);
+actual = @(t) -exp(-10.*t)+cos(t);
+a = 0;
+b = pi;
+ya = 0;
+
+n = 1e4;
+
+[t, y] = implicit_euler(f,a,b,ya,n);
+[j, k] = explicit_euler(f,a,b,ya,n);
+
+hold on
+plot(t, y)
+plot(j, k)
+fplot(actual, [0 pi])
+legend('Implicit','Explicit','Actual')
+hold off
+
+n
+implicitError = sum(abs(actual(t)- y))
+explicitError = sum(abs(actual(j)- k))
